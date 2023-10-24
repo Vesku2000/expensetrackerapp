33f1a93389d7e8867b4d67ffc68bcffd6d52d59c
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-10-24 12:01:37 +0300

    adding screens

---
+++ screens/AllExpenses.js
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+
+function AllExpenses(){
+    return (
+    <Text>
+        All expenses screen
+    </Text>
+    )
+}
+
+export default AllExpenses;
---
+++ screens/ManageExpenses.js
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+
+function ManageExpenses(){
+    return (
+    <Text>
+        managa screen
+    </Text>
+    )
+}
+
+export default ManageExpenses;
